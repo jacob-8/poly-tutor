7e3a022783be3d109cbff54b4853a0b65f9ff9cf
jacob-8 <jwrunner7@gmail.com>
2023-12-07 13:55:31 +0800

    machine translate with i18n-ally

--- site/src/routes/[mother=locale]/[learning=locale]/(app)/+page.svelte
+++ site/src/routes/[mother=locale]/[learning=locale]/(app)/+page.svelte
@@ -7,7 +7,7 @@
   <h1 class="text-3xl mb-3 font-bold">Poly {$page.data.t.layout.tutor}</h1>
 
   <p class="sm:text-lg mb-6 sm:mb-10">
-    Learn Chinese using real content with help from an AI tutor that makes sure you understand what you hearing and reading.
+    {$page.data.t.home.tagline}
   </p>
 
   <div class="grid sm:grid-cols-[repeat(auto-fit,minmax(300px,1fr))] gap-4">
@@ -16,7 +16,7 @@
       <span class="text-lg font-semibold">
         YouTube
       </span>
-      <span class="text-sm my-1">You can choose any show on YouTube, {$page.data.t.layout.tutor} will provide the transcripts.</span>
+      <span class="text-sm my-1">{$page.data.t.home.youtube_description}</span>
       <span class="text-sm font-semibold">
         {$page.data.t.home.watch_shows}
         <span class="i-material-symbols-arrow-right-alt vertical--3px transform group-hover:translate-x-1 transition-all duration-600" />
@@ -26,11 +26,11 @@
     {#if dev}
       <a class="box">{$page.data.t.home.read_texts}</a>
       <a class="box" href="/{$page.data.mother}/{$page.data.learning}/chat">{$page.data.t.home.chat}</a>
-      <a class="box">Read Websites</a>
-      <a class="box">Word Lists</a>
-      <a class="box">Realtime Transcription</a>
-      <a class="box">My Words</a>
-      <a class="box">Dictionary</a>
+      <a class="box">{$page.data.t.home.read_websites}</a>
+      <a class="box">{$page.data.t.home.word_lists}</a>
+      <a class="box">{$page.data.t.home.realtime_transcription}</a>
+      <a class="box">{$page.data.t.home.my_words}</a>
+      <a class="box">{$page.data.t.home.dictionary}</a>
     {/if}
   </div>
 </div>
