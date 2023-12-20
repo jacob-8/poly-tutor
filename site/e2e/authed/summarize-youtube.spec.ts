ef427d7a944a2763785d89d04f1596c20910c0c8
jacob-8 <jwrunner7@gmail.com>
2023-12-20 15:56:04 +0800

    organize specs

--- site/e2e/authed/summarize-youtube.spec.ts
+++ site/e2e/authed/summarize-youtube.spec.ts
@@ -3,15 +3,6 @@ import { seeded_youtubes } from '$lib/mocks/seed/youtubes'
 
 test('user can generate a summary for an entire clip', async ({ page }) => {
   await page.goto(`/en/zh-TW/shows/${seeded_youtubes.zh_transcribed.youtube.id}`)
-  // await page.route('/api/chat', async (route) => {
-  //   const { text } = await route.request().postDataJSON()
-  //   const translatedText = text.split('\n').map(t => 'Mocked translation: ' + t).join('\n')
-  //   route.fulfill({
-  //     status: 200,
-  //     headers: { 'Content-Type': 'application/json' },
-  //     body: JSON.stringify({ line_separated_translations: translatedText })
-  //   })
-  // })
-  // await page.getByRole('button', { name: 'Summarize' }).click()
-  await expect(page.getByText('This is a fake summary')).toBeVisible()
+  await page.getByRole('button', { name: 'Summarize' }).click()
+  await expect(page.getByText('This is a mocked streaming response')).toBeVisible()
 })
