ba6510c994c4701f62b3c47733233581af82f104
jacob-8 <jwrunner7@gmail.com>
2024-01-25 16:34:59 +0800

    show voices even if has none of the defaults

--- site/src/lib/utils/speak.ts
+++ site/src/lib/utils/speak.ts
@@ -6,18 +6,25 @@ export const current_voice = writable<SpeechSynthesisVoice>(null)
 let available_voices: SpeechSynthesisVoice[] = []
 
 const default_en_voices = [
+  // Windows
   'Google UK English Male',
   'Google UK English Female',
   'Google US English',
   'Microsoft Mark - English (United States)',
+
+  // Android
+  'English United Kingdom',
+  'English United States',
 ]
 
 // const default_zh_CN_voices = [
 //   'Google 普通话（中国大陆）',
+//   'Chinese China',
 // ]
 
 // const default_zh_TW_voices = [
 //   'Google 國語（臺灣）',
+//   'Chinese Taiwan',
 // ]
 
 const PREFERRED_VOICE_KEY = 'preferred_voice_name'
@@ -27,22 +34,24 @@ if (browser) load_preferred_voice()
 function load_preferred_voice() {
   const stored_voice_name = localStorage.getItem(PREFERRED_VOICE_KEY)
 
+  window.speechSynthesis.getVoices() // warm things up on mobile
   window.speechSynthesis.onvoiceschanged = function() {
     available_voices = window.speechSynthesis.getVoices()
+    if (!available_voices.length) return
     console.info({voices: available_voices.map(({name}) => name)})
 
     const preferred_voice = available_voices.find(voice => voice.name === stored_voice_name)
-    if (preferred_voice) {
-      current_voice.set(preferred_voice)
-    } else {
-      for (const voice_name of default_en_voices) {
-        const default_voice = available_voices.find(voice => voice.name === voice_name)
-        if (default_voice) {
-          current_voice.set(default_voice)
-          break
-        }
+    if (preferred_voice)
+      return current_voice.set(preferred_voice)
+
+    for (const voice_name of default_en_voices) {
+      const default_voice = available_voices.find(voice => voice.name === voice_name)
+      if (default_voice) {
+        current_voice.set(default_voice)
+        return
       }
     }
+    current_voice.set(available_voices[0])
   }
 }
 
