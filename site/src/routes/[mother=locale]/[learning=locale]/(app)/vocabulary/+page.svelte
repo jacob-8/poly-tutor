a4004649f1ae55c4a12244c39e8a8cead67c9196
jacob-8 <jwrunner7@gmail.com>
2024-02-03 15:26:19 +0800

    list top unknown words

--- site/src/routes/[mother=locale]/[learning=locale]/(app)/vocabulary/+page.svelte
+++ site/src/routes/[mother=locale]/[learning=locale]/(app)/vocabulary/+page.svelte
@@ -1,24 +1,59 @@
 <script lang="ts">
-  import type { SyncVocabRequestBody, SyncVocabResponseBody } from '$api/sync-vocab/+server'
-  import { post_request } from '$lib/utils/post-request'
+  import { WordStatus } from '$lib/types'
+  // import type { SyncVocabRequestBody, SyncVocabResponseBody } from '$api/sync-vocab/+server'
+  // import { post_request } from '$lib/utils/post-request'
   import type { PageData } from './$types'
 
   export let data: PageData
-  $: ({user_vocabulary, user} = data)
+  $: ({user_vocabulary} = data)
 
-  async function sync_vocab() {
-    const url = prompt('Enter url')
-    if (!url) return
-    const { data, error } = await post_request<SyncVocabRequestBody, SyncVocabResponseBody>('/api/sync-vocab', { url })
-    console.info({data, error})
-  }
+  // async function sync_vocab() {
+  //   const url = prompt('Enter url')
+  //   if (!url) return
+  //   const { data, error } = await post_request<SyncVocabRequestBody, SyncVocabResponseBody>('/api/sync-vocab', { url })
+  //   console.info({data, error})
+  // }
+
+  $: unknown_words = Object.entries($user_vocabulary)
+    .filter(([_word, {status}]) => status === WordStatus.unknown)
+    .sort((a, b) => b[1].views - a[1].views)
+    .map(([word, {views}]) => ({word, views}))
+
+  $: pronunciation_words = Object.entries($user_vocabulary)
+    .filter(([_word, {status}]) => status === WordStatus.pronunciation)
+    .sort((a, b) => b[1].views - a[1].views)
+    .map(([word, {views}]) => ({word, views}))
+
+  $: tone_words = Object.entries($user_vocabulary)
+    .filter(([_word, {status}]) => status === WordStatus.tone)
+    .sort((a, b) => b[1].views - a[1].views)
+    .map(([word, {views}]) => ({word, views}))
+
+  $: known_words = Object.entries($user_vocabulary)
+    .filter(([_word, {status}]) => status === WordStatus.known)
+    .sort((a, b) => b[1].views - a[1].views)
+    .map(([word, {views}]) => ({word, views}))
 </script>
 
-{#if $user?.email === 'jacob@polylingual.dev'}
+<!-- {#if $user?.email === 'jacob@polylingual.dev'}
   <button type="button" on:click={sync_vocab}>Migrate Vocab</button>
-{/if}
+{/if} -->
+
+<div class="p-2">
+  <div>
+    Unknown: {unknown_words.length},
+    Pronunciation: {pronunciation_words.length},
+    Tone: {tone_words.length},
+    Known: {known_words.length},
+    Total: {Object.keys($user_vocabulary).length}
+  </div>
+
+  {#each unknown_words.slice(0, 100) as word}
+    <div>{word.word} - {word.views}</div>
+
+  {/each}
 
 
-Count: {Object.keys($user_vocabulary).length}
-<hr>
-<pre>{JSON.stringify($user_vocabulary, null, 2)}</pre>
+  <!-- <pre>{JSON.stringify($user_vocabulary, null, 2)}</pre> -->
+
+</div>
