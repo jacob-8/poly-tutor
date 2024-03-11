7b99709a266706cc89cd6de40c4853549f67cc4f
jacob-8 <jwrunner7@gmail.com>
2024-03-11 13:37:07 +0800

    feat: add google analytics

---
+++ site/src/routes/[mother=locale]/[learning=locale]/(app)/Analytics.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import { partytownSnippet } from '@builder.io/partytown/integration'
+</script>
+
+  <svelte:head>
+    <script>
+      partytown = {
+      forward: ['dataLayer.push']
+      };
+    </script>
+
+    {@html '<script>' + partytownSnippet() + '</script>'}
+
+    <script type="text/partytown" src="https://www.googletagmanager.com/gtag/js?id=G-ECHKC85GK4"></script>
+    <script type="text/partytown">
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-ECHKC85GK4');
+    </script>
+  </svelte:head>
