1500566d1b9cbd5e35d4ae45f9c6bd2d519a49e5
jacob-8 <jwrunner7@gmail.com>
2024-03-11 13:42:17 +0800

    don't use partytown to test integration

--- site/src/routes/[mother=locale]/[learning=locale]/(app)/Analytics.svelte
+++ site/src/routes/[mother=locale]/[learning=locale]/(app)/Analytics.svelte
@@ -1,21 +1,22 @@
 <script lang="ts">
-  import { partytownSnippet } from '@builder.io/partytown/integration'
+// import { partytownSnippet } from '@builder.io/partytown/integration'
 </script>
 
-  <svelte:head>
-    <script>
+<svelte:head>
+  <!-- <script>
       partytown = {
       forward: ['dataLayer.push']
       };
     </script>
 
-    {@html '<script>' + partytownSnippet() + '</script>'}
+    {@html '<script>' + partytownSnippet() + '</script>'} -->
 
-    <script type="text/partytown" src="https://www.googletagmanager.com/gtag/js?id=G-ECHKC85GK4"></script>
-    <script type="text/partytown">
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-      gtag('config', 'G-ECHKC85GK4');
-    </script>
-  </svelte:head>
+  <!-- type="text/partytown" -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-ECHKC85GK4"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-ECHKC85GK4');
+  </script>
+</svelte:head>
